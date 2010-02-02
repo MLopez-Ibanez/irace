6b3dca26079b4cf930f0bac6c1bbb6d6d2235b01
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2010-02-02 17:21:17 +0000

    * dist.sh: Exclude some files.
    git-svn-id: file:///home/manu/work/irace/svn/trunk@31 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- dist.sh
+++ dist.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 set -u
+
+cat > .excludes <<EOF
+.excludes
+dist.sh
+TUNE*
+Program
+Instances/*
+.svn
+parameters.txt
+tune-conf
+*.diff
+hooks/hook-run
+hooks/hook-instance-finished
+hooks/tune-main
+EOF
+
 BASEDIR=`pwd`
 VERSION=`svnversion -n .`
 VERSION=svn${VERSION}
@@ -8,7 +24,8 @@ VERSION=svn${VERSION}
 DIST_SRC=ifrace-${VERSION}
 echo "ifrace: version $VERSION"
 mkdir -p ../${DIST_SRC}
-rsync -rlpC --exclude=dist.sh --exclude=Program --exclude='Instances/*' --exclude=.svn . ../${DIST_SRC}/
+rsync -rlpC --exclude-from=.excludes . ../${DIST_SRC}/
+rm -f .excludes
 cd ..
 tar cf - ${DIST_SRC} | gzip -f9 > ${DIST_SRC}.tar.gz 
 cd ${BASEDIR}
