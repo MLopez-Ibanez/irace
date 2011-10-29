b3c18adf262855743efb0e561851805619d07a48
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2011-10-29 18:42:47 +0000

    * DESCRIPTION, .Rbuilignore: Files needed to build a package.  * dist.sh: Update.
    git-svn-id: file:///home/manu/work/irace/svn/trunk@474 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- dist.sh
+++ dist.sh
@@ -2,36 +2,13 @@
 set -e
 set -u
 
-cat > .excludes <<'EOF'
-Makefile
-.excludes
-.*
-dist.sh
-TUNE*
-Program
-Instances/*
-.svn
-parameters.txt
-tune-conf
-*.diff
-hooks/hook-run
-hooks/hook-evaluate
-hooks/tune-main
-doc/
-TODO
-examples/beamaco
-examples/hypervolume
-web/
-EOF
-
 BASEDIR=`pwd`
 VERSION=$(cat VERSION)
 #VERSION=`date '+%F' | tr -d '\n'`
 DIST_SRC=irace-${VERSION}
 echo "irace: version $VERSION"
 mkdir -p ../${DIST_SRC}
-rsync -rlpC --exclude-from=.excludes . ../${DIST_SRC}/
-rm -f .excludes
+rsync -rlpC --exclude-from=.Rbuildignore . ../${DIST_SRC}/
 cd ..
 (tar cf - ${DIST_SRC} | gzip -f9 > ${DIST_SRC}.tar.gz) && rm -rf ${DIST_SRC}
 cd ${BASEDIR}
