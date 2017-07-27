9a82995be3f0e97aad295da4674b1c8e4cca0076
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2017-07-27 09:19:59 +0000

    * R/readParameters.R: Fixme.
    git-svn-id: file:///home/manu/work/irace/svn/trunk@1835 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- R/readParameters.R
+++ R/readParameters.R
@@ -130,9 +130,6 @@ readParameters <- function (file, digits = 4, debugLevel = 0, text)
                  " at ", filename, ", line ", line, context)
   }
   
-  # *************************************************************************
-  # FIXME: Only domain needs to be a list, the rest should be
-  # vectors, which will make many operations way faster.
   parameters <- list(names = c(),
                      types = c(),
                      switches = c(),
@@ -207,6 +204,8 @@ readParameters <- function (file, digits = 4, debugLevel = 0, text)
       }
 
       if (param.type == "r") {
+        # FIXME: Given (0.01,0.99) and digits=1, this produces (0, 1), which is
+        # probably not what the user wants.
         param.value <- round(param.value, digits = digits)
       } else if (param.type == "i" && !all(is.wholenumber(param.value))) {
         errReadParameters (filename, nbLines, NULL,
