4e04020a6f78362bc80b9ae388ce9ae284a03d0a
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2018-06-14 19:55:03 +0000

    * R/testing.R: Add some error checking and FIXME notes.
    git-svn-id: file:///home/manu/work/irace/svn/trunk@2067 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- R/testing.R
+++ R/testing.R
@@ -31,6 +31,8 @@ testConfigurations <- function(configurations, scenario, parameters)
   
   testInstances <- scenario$testInstances
   instances.ID <- names(testInstances)
+  if (length(testInstances) == 0) irace.error("No test instances given")
+  if (is.null(instances.ID)) irace.error("testInstances must have names")
   
   # 2147483647 is the maximum value for a 32-bit signed integer.
   # We use replace = TRUE, because replace = FALSE allocates memory for each possible number.
@@ -67,11 +69,14 @@ testConfigurations <- function(configurations, scenario, parameters)
     target.output <- execute.evaluator (experiments, scenario, target.output,
                                         configurations$.ID.)
 
+  # FIXME: It would be much faster to get convert target.output$cost to a
+  # vector, then initialize the matrix with the vector.
   testResults <- matrix(NA, ncol = nrow(configurations), nrow = length(testInstances),
                         # dimnames = list(rownames, colnames)
                         dimnames = list (instances.ID, configurations$.ID.))
 
-  # FIXME: It would be much faster to get a vector cost, applyPAR to it, then assign it.
+  # FIXME: It would be much faster to get a vector cost, applyPAR to it, then
+  # assign it.
   for (i in seq_along(experiments)) {
     cost <- target.output[[i]]$cost
     if (scenario$capping) cost <- applyPAR(cost, scenario)
