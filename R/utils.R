cff3b928d7a1fd2a37269da09876d6293e593c32
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-04-18 13:24:41 +0000

    * R/utils.R: Check length first to avoid error length > 1 in coercion to logical
    git-svn-id: file:///home/manu/work/irace/svn/trunk@2225 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- R/utils.R
+++ R/utils.R
@@ -194,7 +194,7 @@ is.null.or.na <- function(x)
 
 is.null.or.empty <- function(x)
 {
-  (length(x) == 0) || (!suppressWarnings(is.na(x)) && length(x) == 1 && is.character(x) && x == "")
+  (length(x) == 0) || (length(x) == 1 && !suppressWarnings(is.na(x)) && is.character(x) && x == "")
 }
 
 is.function.name <- function(FUN)
