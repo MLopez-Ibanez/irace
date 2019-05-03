cb02e5879cab20199615ffb5907df418afb238a0
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-05-03 19:45:42 +0000

    rename src/iracexe src/iracebin
    git-svn-id: file:///home/manu/work/irace/svn/trunk@2244 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,4 +1,4 @@
-execs <- c(file.path("iracexe", "irace"))
+execs <- c(file.path("iracbin", "irace"))
 if (WINDOWS) execs <- paste0(execs, ".exe")
 if (any(file.exists(execs))) {
   dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
