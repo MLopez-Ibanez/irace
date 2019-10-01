214bf3fcd59dabd8419f925519cedc744672037e
leslieperez <9088287+leslieperez@users.noreply.github.com>
2019-10-01 21:52:36 +0000

    test-mlr.R fix version mlr package dependency problem
    git-svn-id: file:///home/manu/work/irace/svn/trunk@2329 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- tests/testthat/test-mlr.R
+++ tests/testthat/test-mlr.R
@@ -2,15 +2,16 @@ context("mlr")
 
 test_that("mlr", {
   skip_on_cran()
-  suppressWarnings(skip_if_not_installed("mlr", minimum_version = "2.14.1"))
+  suppressWarnings(skip_if_not_installed("mlr", minimum_version = "2.15.0"))
+  require(mlr, quietly = TRUE)
 
-  ps = mlr::makeParamSet(
-    mlr::makeNumericParam("cp", lower = 0.1, upper = 1),
-    mlr::makeIntegerParam("minsplit", lower = 1, upper = 10)
+  ps = ParamHelpers::makeParamSet(
+    ParamHelpers::makeNumericParam("cp", lower = 0.1, upper = 1),
+    ParamHelpers::makeIntegerParam("minsplit", lower = 1, upper = 10)
   )
   lrn = mlr::makeLearner("classif.rpart")
   n = 30
   ctrl = mlr::makeTuneControlIrace(maxExperiments = n, nbIterations = 1L, minNbSurvival = 1)
-  tr = mlr::tuneParams(lrn, iris.task, hout, par.set = ps, control = ctrl)
+  tr = mlr::tuneParams(lrn, mlr::iris.task, mlr::hout, par.set = ps, control = ctrl)
   expect_true(TRUE)
 })
