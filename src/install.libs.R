ff5d98fb8817e2aeb1b37242990470a85fa0e3a6
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-01-21 23:01:41 +0000

    * src/install.libs.R: Do not print dest.
    git-svn-id: file:///home/manu/work/irace/svn/trunk@2411 d10a546b-4b69-48e5-8b66-49ccea70c6a4

--- src/install.libs.R
+++ src/install.libs.R
@@ -2,7 +2,7 @@ execs <- file.path("iracebin", c("irace","irace2pyimp"))
 if (WINDOWS) execs <- paste0(execs, ".exe")
 if (any(file.exists(execs))) {
   dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
-  print(dest)
+  #print(dest)
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(execs, dest, overwrite = TRUE)
 } else {
