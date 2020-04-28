95051371d3da9f9707e3f4a728d2bf75dff572ed
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-04-28 15:20:10 +0100

    * tests/testthat/test-bugs.R: Skip in R >= 4.

--- tests/testthat/test-bugs.R
+++ tests/testthat/test-bugs.R
@@ -6,6 +6,7 @@ run_irace_from_rds <- function(rds_filename)
 {
   # FIXME: For some unknown reason, this test doesn't work in R 3.6
   skip_if(grepl("^3.6", getRversion()))
+  skip_if(grepl("^4", getRversion()))
 
   scenario <- readRDS(rds_filename)$scenario
   parameters <- readRDS(rds_filename)$parameters
@@ -15,7 +16,7 @@ run_irace_from_rds <- function(rds_filename)
     row <- which(saved$instancesList[, "instance"] == experiment[["id.instance"]]
                  & saved$instancesList[, "seed"] == experiment[["seed"]])
     cost <- saved$experiments[row, experiment[["id.configuration"]] ]
-    if (is.na(cost)) {
+    if (length(cost) == 0 || is.na(cost)) {
       print(row)
       print(experiment)
     }
@@ -30,6 +31,7 @@ test_that("bug_large_new_instances", {
   skip_on_cran()
   # FIXME: For some unknown reason, this test doesn't work in R 3.6
   skip_if(grepl("^3.6", getRversion()))
+  skip_if(grepl("^4", getRversion()))
   # FIXME: Convert this test to use run_irace_from_rds()
   load("bug_large_new_instances.Rdata", verbose = TRUE)
 
@@ -39,7 +41,7 @@ test_that("bug_large_new_instances", {
     row <- which(saved_instances_list[, "instance"] == experiment[["id.instance"]]
                  & saved_instances_list[, "seed"] == experiment[["seed"]])
     cost <- saved_experiments[row, experiment[["id.configuration"]] ]
-    if (is.na(cost)) {
+    if (length(cost) == 0 || is.na(cost)) {
       print(row)
       print(experiment)
     }
