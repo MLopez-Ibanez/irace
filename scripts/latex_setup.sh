ab4fa0af7e55dd64915e6769bd2a5c0911ffa65a
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-05-16 11:09:01 +0100

    * scripts/latex_setup.sh: Latex setup required for building vignettes with Travis.

---
+++ scripts/latex_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+tlmgr option -- autobackup 0
+tlmgr option -- docfiles 0
+tlmgr option -- srcfiles 0
+#tlmgr option -- require-verification 0
+#tlmgr option -- verify-downloads 0
+#tlmgr option -- verify-repo none
+tlmgr install \
+      oberdiek \
+      epstopdf-pkg \
+      epstopdf \
+      grfext \
+      gettitlestring \
+      environ \
+      trimspaces \
+      etoolbox \
+      upquote \
+      babel-english \
+      pgf \
+      xcolor \
+      algorithms \
+      relsize \
+      tocbibind \
+      appendix \
+      tocloft \
+      enumitem \
+      listings \
+      tcolorbox \
+      collection-fontsrecommended \
+      framed \
+      inconsolata \
+      microtype
+
