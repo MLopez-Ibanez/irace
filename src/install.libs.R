e2c91270dcf933cc13351914b736b5c57f603699
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-05-21 16:47:56 +0100

    * Move irace2pyimp to its own package.

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,4 +1,4 @@
-execs <- file.path("iracebin", c("irace","irace2pyimp"))
+execs <- file.path("iracebin", c("irace"))
 if (WINDOWS) execs <- paste0(execs, ".exe")
 if (any(file.exists(execs))) {
   dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
