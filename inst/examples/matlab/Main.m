b7225399ab4f46ef83d15c55dcb7f4ef1aeb523d
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-07-23 12:22:46 +0200

    * inst/examples/matlab/: More improvements.

--- inst/examples/matlab/Main.m
+++ inst/examples/matlab/Main.m
@@ -1,15 +1,19 @@
-% function Result = Main(INSTANCE, A, B)
+% function Result = Main(INSTANCE,SEED,A, B)
 % Using INSTANCE as a seed, this function creates a pseudo-random number 
 % drawn from a uniform distribution in the open interval (-1,1) and returns
 % the value of Result, where Result = A*B + r
-function Result = Main(INSTANCE,A,B)
+function Result = Main(INSTANCE,SEED,A,B)
 
 rng('default');
-rng(INSTANCE);
+rng(SEED);
 
 minValue = -1;
 maxValue = 1;
 r = (maxValue - minValue) * rand(1) + minValue;
 Result = A * B + r;
-fprintf('Result for irace=%g\n', Result);
+% fprintf('Result for irace=%g\n', Result);
+fid=fopen('Result.txt','wt');
+fprintf(fid,'%g\n',Result);
+fclose(fid);
+
 end
