277ca51645770eb140e7ab4e5e563cec5c996f32
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-05-12 13:45:59 +0100

    * tests/testthat/setup.R,tests/testthat/teardown.R: Add warning options.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,8 @@
 old_opts = options(
+  warnPartialMatchArgs = TRUE,
   warnPartialMatchAttr = TRUE,
-  warnPartialMatchDollar = TRUE,
-  warnPartialMatchArgs = TRUE
+  warnPartialMatchDollar = TRUE
 )
+
+old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
+
