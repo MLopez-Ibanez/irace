a5fb32db33d3c5f6e959253e7e03799c88e3b873
Deyao Chen <chendeyao000@gmail.com>
2022-11-23 15:40:06 +0000

    allow read configuration to read directly from data.frame

--- R/readConfiguration.R
+++ R/readConfiguration.R
@@ -8,6 +8,7 @@
 #' @template arg_parameters
 #' @template arg_debuglevel
 #' @template arg_text
+#' @param configurationTable (`data.frame`) Data for the configuration as a data.frame. 
 #' 
 #' @return A data frame containing the obtained configurations. 
 #'   Each row of the data frame is a candidate configuration, 
@@ -32,18 +33,20 @@
 #' @author Manuel López-Ibáñez and Jérémie Dubois-Lacoste
 #' @export
 ## FIXME: What about digits?
-readConfigurationsFile <- function(filename, parameters, debugLevel = 0, text)
+readConfigurationsFile <- function(filename, parameters, debugLevel = 0, text, configurationTable)
 {
-  if (missing(filename) && !missing(text)) {
+  if (missing(configurationTable) & missing(filename) && !missing(text)) {
     filename <- strcat("text=", deparse(substitute(text)))
     configurationTable <- read.table(text = text, header = TRUE,
                                      colClasses = "character",
                                      stringsAsFactors = FALSE)
-  } else {
+  } else if (missing(configurationTable) & !missing(filename)) {
     # Read the file.
     configurationTable <- read.table(filename, header = TRUE,
                                      colClasses = "character",
                                      stringsAsFactors = FALSE)
+  } else if (!missing(configurationTable)) {
+    filename <- strcat("configurationTable=", deparse(substitute(configurationTable)))
   }
   irace.assert(is.data.frame(configurationTable))
   nbConfigurations <- nrow(configurationTable)
