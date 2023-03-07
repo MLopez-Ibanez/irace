fe03e2aee35f5eab1fa2ee2a7976ddc2565b4e3f
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-03-07 17:14:38 +0000

    * tests/testthat/test-readconfs.R: Minor cleanup.

--- tests/testthat/test-readconfs.R
+++ tests/testthat/test-readconfs.R
@@ -3,9 +3,9 @@ context("Test read configurations")
 withr::with_output_sink("test-readconfs.Rout", {
 
 test_that("checkDuplicates", {
-  params <- irace:::readParameters("parameters.txt")
+  params <- irace::readParameters("parameters.txt")
   
-  expect_error(irace:::readConfigurationsFile(text='
+  expect_error(irace::readConfigurationsFile(text='
 param1 param2 mode   real mutation
 5        NA    "x2"   4.0   "low"
 1        NA    "x2"   4.0   "low"
@@ -13,7 +13,7 @@ param1 param2 mode   real mutation
 NA        NA   "x3"   4.5   "low"
 ', parameters = params), "Duplicated")
 
-  expect_error(irace:::readConfigurationsFile(text='
+  expect_error(irace::readConfigurationsFile(text='
 param1 param2 mode   real mutation
 5        NA    "x2"   4.0   "low"
 1        NA    "x2"   4.0   "low"
