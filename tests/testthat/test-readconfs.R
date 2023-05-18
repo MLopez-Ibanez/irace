c7bb4d0dd995190aebdf11b65ba81c251b8a657f
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-05-18 18:41:56 +0100

    * * tests/testthat/test-readconfs.R: Test handling of fixed parameters.

--- tests/testthat/test-readconfs.R
+++ tests/testthat/test-readconfs.R
@@ -5,7 +5,24 @@ params <- irace::readParameters("parameters.txt")
 expect_error_readconfs <- function(table, exp)
   expect_error(irace::readConfigurationsFile(text=table, parameters = params), exp)
 
-test_that("checkDuplicates", {
+  test_that("no error", {
+    x <- irace::readConfigurationsFile(text='
+param1 param2 mode   real mutation
+5        NA    "x2"   4.0   "low"
+1        NA    "x2"   4.0   "low"
+NA       NA   "x3"   4.5   "low"', parameters=params)
+    expect_equal(nrow(x), 3L)
+    expect_equal(ncol(x), 6L) # It adds the fixed column
+  })
+  test_that("wrong fixed", {
+    expect_error_readconfs('
+param1 param2 mode   real mutation fixed
+5        NA    "x2"   4.0   "low"  fixed
+1        NA    "x2"   4.0   "low"  fixed
+NA       NA   "x3"   4.5   "low"   wrong', "is not among the valid values")
+  })
+  
+  test_that("checkDuplicates", {
   expect_error_readconfs('
 param1 param2 mode   real mutation
 5        NA    "x2"   4.0   "low"
