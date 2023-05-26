5b0a7cc6d0f762245f7c90720322a0c4f7894f18
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-05-26 17:27:08 +0100

    * R/tnorm.R: Simplify.

--- R/tnorm.R
+++ R/tnorm.R
@@ -44,77 +44,67 @@
 ## Rejection sampling algorithm by Robert (Stat. Comp (1995), 5, 121-5)
 ## for simulating from the truncated normal distribution.
 
-rtnorm <- function (n, mean = 0, sd = 1, lower = -Inf, upper = Inf) {
-    if (length(n) > 1)
-        n <- length(n)
-    mean <- rep_len(mean, n)
-    sd <- pmax.int(sd, 1e-15) # Small values of sd break the function.
-    sd <- rep_len(sd, n)
-    lower <- rep_len(lower, n)
-    upper <- rep_len(upper, n)
-    lower <- (lower - mean) / sd ## Algorithm works on mean 0, sd 1 scale
-    upper <- (upper - mean) / sd
-    ind <- seq_len(n)
-    ret <- numeric(n)
-    nas <- is.na(mean) | is.na(sd) | is.na(lower) | is.na(upper)
-    if (any(nas)) warning("NAs produced")
-    ## Different algorithms depending on where upper/lower limits lie.
-    alg <- ifelse(
-                  ((lower > upper) | nas),
-                  -1,# return NaN
-                  ifelse(
-                         ((lower < 0 & upper == Inf) |
-                          (lower == -Inf & upper > 0) |
-                          (is.finite(lower) & is.finite(upper) & (lower < 0) & (upper > 0) & (upper-lower > sqrt(2*pi)))
-                          ),
-                         0, # standard "simulate from normal and reject if outside limits" method. Use if bounds are wide.
-                         ifelse(
-                                (lower >= 0 & (upper > lower + 2*sqrt(exp(1)) /
-                                 (lower + sqrt(lower^2 + 4)) * exp((lower*2 - lower*sqrt(lower^2 + 4)) / 4))),
-                                1, # rejection sampling with exponential proposal. Use if lower >> mean
-                                ifelse(upper <= 0 & (-lower > -upper + 2*sqrt(exp(1)) /
-                                       (-upper + sqrt(upper^2 + 4)) * exp((upper*2 - -upper*sqrt(upper^2 + 4)) / 4)),
-                                       2, # rejection sampling with exponential proposal. Use if upper << mean.
-                                       3)))) # rejection sampling with uniform proposal. Use if bounds are narrow and central.
-
-    ind.nan <- ind[alg==-1]; ind.no <- ind[alg==0]; ind.expl <- ind[alg==1]; ind.expu <- ind[alg==2]; ind.u <- ind[alg==3]
-    ret[ind.nan] <- NaN
+rtnorm <- function (n, mean = 0, sd = 1, lower, upper) {
+  stopifnot(length(n) == 1L && length(mean) == 1L
+            && length(sd) == 1L && length(lower) == 1L && length(upper) == 1L)
+  stopifnot(is.finite(lower) && is.finite(upper))
+  lower <- (lower - mean) / sd # Algorithm works on mean 0, sd 1 scale
+  upper <- (upper - mean) / sd
+  sdzero <- (sd < .Machine$double.eps)
+  nas <- is.na(mean) || is.na(sd) || (sdzero && (lower > mean || mean > upper))
+  ret <- rep_len(NaN, n)
+  if (nas) {
+    warning("NAs produced")
+    return(ret) # return NaN
+  } else if (lower > upper) {
+    return(ret) # return NaN
+  } else if (sdzero) {
+    return(rep_len(mean, n)) # SD zero, so set the sampled value to the mean. 
+  } else if ((lower < 0) && (upper > 0) && (upper - lower > sqrt(2*pi))) {
+    # standard "simulate from normal and reject if outside limits" method. Use if bounds are wide.
+    ind.no <- seq_len(n)
     while (length(ind.no) > 0) {
-        y <- rnorm(length(ind.no))
-        done <- which(y >= lower[ind.no] & y <= upper[ind.no])
-        ret[ind.no[done]] <- y[done]
-        ind.no <- setdiff(ind.no, ind.no[done])
+      y <- rnorm(length(ind.no))
+      done <- (y >= lower & y <= upper)
+      ret[ind.no[done]] <- y[done]
+      ind.no <- ind.no[!done]
     }
-    stopifnot(length(ind.no) == 0)
+  } else if (lower >= 0 && (upper > lower + 2*sqrt(exp(1)) /
+                            (lower + sqrt(lower^2 + 4)) * exp((lower*2 - lower*sqrt(lower^2 + 4)) / 4))) {
+    # rejection sampling with exponential proposal. Use if lower >> mean
+    ind.expl <- seq_len(n)
+    a <- (sqrt(lower^2 + 4) + lower) / 2
     while (length(ind.expl) > 0) {
-        a <- (lower[ind.expl] + sqrt(lower[ind.expl]^2 + 4)) / 2
-        z <- rexp(length(ind.expl), a) + lower[ind.expl]
-        u <- runif(length(ind.expl))
-        done <- which((u <= exp(-(z - a)^2 / 2)) & (z <= upper[ind.expl]))
-        ret[ind.expl[done]] <- z[done]
-        ind.expl <- setdiff(ind.expl, ind.expl[done])
+      z <- rexp(length(ind.expl), a) + lower
+      u <- runif(length(ind.expl))
+      done <- (u <= exp(-(z - a)^2 / 2)) & (z <= upper)
+      ret[ind.expl[done]] <- z[done]
+      ind.expl <- ind.expl[!done]
     }
-    stopifnot(length(ind.expl) == 0)
+  } else if (upper <= 0 && (-lower > -upper + 2*sqrt(exp(1)) /
+                            (-upper + sqrt(upper^2 + 4)) * exp((upper*2 - -upper*sqrt(upper^2 + 4)) / 4))) {
+    # rejection sampling with exponential proposal. Use if upper << mean.
+    ind.expu <- seq_len(n)
+    a <- (sqrt(upper^2 +4) - upper) / 2
     while (length(ind.expu) > 0) {
-        a <- (-upper[ind.expu] + sqrt(upper[ind.expu]^2 +4)) / 2
-        z <- rexp(length(ind.expu), a) - upper[ind.expu]
-        u <- runif(length(ind.expu))
-        done <- which((u <= exp(-(z - a)^2 / 2)) & (z <= -lower[ind.expu]))
-        ret[ind.expu[done]] <- -z[done]
-        ind.expu <- setdiff(ind.expu, ind.expu[done])
+      z <- rexp(length(ind.expu), a) - upper
+      u <- runif(length(ind.expu))
+      done <- (u <= exp(-(z - a)^2 / 2)) & (z <= -lower)
+      ret[ind.expu[done]] <- -z[done]
+      ind.expu <- ind.expu[!done]
     }
-    stopifnot(length(ind.expu) == 0)
+  } else {
+    # rejection sampling with uniform proposal. Use if bounds are narrow and central.
+    ind.u <- seq_len(n)
+    K <- if (lower > 0) lower^2 else if (upper < 0) upper^2 else 0
     while (length(ind.u) > 0) {
-        z <- runif(length(ind.u), lower[ind.u], upper[ind.u])
-        rho <- ifelse(lower[ind.u] > 0,
-                      exp((lower[ind.u]^2 - z^2) / 2), ifelse(upper[ind.u] < 0,
-                                                            exp((upper[ind.u]^2 - z^2) / 2),
-                                                            exp(-z^2/2)))
-        u <- runif(length(ind.u))
-        done <- which(u <= rho)
-        ret[ind.u[done]] <- z[done]
-        ind.u <- setdiff(ind.u, ind.u[done])
+      z <- runif(length(ind.u), lower, upper)
+      rho <- exp((K - z^2) / 2)
+      u <- runif(length(ind.u))
+      done <- (u <= rho)
+      ret[ind.u[done]] <- z[done]
+      ind.u <- ind.u[!done]
     }
-    stopifnot(length(ind.u) == 0)
-    ret*sd + mean
+  }        
+  ret*sd + mean
 }
