ad375cfae8e62c8110a1e44e8b1fad35a19d8803
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-06-02 10:08:05 +0100

    * R/parametersAnalysis.R: Some minor improvements.

--- R/parameterAnalysis.R
+++ R/parameterAnalysis.R
@@ -3,9 +3,7 @@
 #' @template arg_iraceresults
 #' @param n Number of elite configurations to return, if \code{n} is larger than the 
 #' number of configurations, then only the existing ones are returned. The default (\code{n=0}) returns all of them.
-#' @param drop.metadata Remove metadata, such the configuration ID and
-#' the ID of the parent, from the returned configurations. See
-#' \code{\link{removeConfigurationsMetaData}}.
+#' @template arg_drop_metadata
 #' 
 #' @return A data frame containing the elite configurations required.
 #'
@@ -23,7 +21,7 @@ getFinalElites <- function(iraceResults, n = 0L, drop.metadata = FALSE)
     
   last.elites <- iraceResults$allElites[[length(iraceResults$allElites)]]
   
-  if (n == 0)
+  if (n == 0L)
     n <- length(last.elites) 
     
   if (length(last.elites) < n) {
@@ -37,16 +35,14 @@ getFinalElites <- function(iraceResults, n = 0L, drop.metadata = FALSE)
                            drop = FALSE)
   if (drop.metadata)
     configurations <- removeConfigurationsMetaData(configurations)
-  return(configurations)
+  configurations
 }
 
 #' Returns the configurations selected by ID.
 #' 
 #' @template arg_iraceresults
 #' @param ids The id or a vector of ids of the candidates configurations to obtain.
-#' @param drop.metadata Remove metadata, such the configuration ID and
-#' the ID of the parent, from the returned configurations. See
-#' [removeConfigurationsMetaData()].
+#' @template arg_drop_metadata
 #' 
 #' @return A data frame containing the elite configurations required.
 #'
@@ -58,17 +54,9 @@ getConfigurationById <- function(iraceResults, ids, drop.metadata = FALSE)
   if (missing(iraceResults)) stop("argument 'iraceResults' is missing")
   iraceResults <- read_logfile(iraceResults)
     
-  if (length(ids) < 1) stop("You must provide at least one configuration id.")
-  
-  selection <- iraceResults$allConfigurations[,".ID."] %in% ids
-  
-  if (length(selection) < 1) stop("No configuration found with id", ids,".")
-  
-  configurations <-iraceResults$allConfigurations[selection, , drop = FALSE]
-  
-  if (drop.metadata)
-    configurations <- removeConfigurationsMetaData(configurations)
-  return(configurations)
+  if (length(ids) < 1L) stop("You must provide at least one configuration id.")
+
+  get_configuration_by_id_helper (iraceResults$allConfigurations, ids, drop.metadata = drop.metadata)
 }
 
 #' Returns the configurations by the iteration in which they were executed.
@@ -76,9 +64,7 @@ getConfigurationById <- function(iraceResults, ids, drop.metadata = FALSE)
 #' @template arg_iraceresults
 #' @param iterations The iteration number or a vector of iteration numbers from where 
 #'  the configurations should be obtained.
-#' @param drop.metadata (\code{FALSE}) Remove metadata, such the configuration ID and
-#' the ID of the parent, from the returned configurations. See
-#' \code{\link{removeConfigurationsMetaData}}.
+#' @template arg_drop_metadata
 #' 
 #' @return A data frame containing the elite configurations required.
 #'
@@ -91,23 +77,26 @@ getConfigurationByIteration <- function(iraceResults,
   if (missing(iraceResults)) stop("argument 'iraceResults' is missing")
   iraceResults <- read_logfile(iraceResults)
 
-  if (length(iterations) < 1)
-    stop("You must provide at least one configuration id.")
+  if (length(iterations) < 1L)
+    stop("You must provide at least one configuration ID.")
 
   # To silence warning.
   iteration <- NULL
   ids <- unique(subset(as.data.frame(iraceResults$experimentLog),
                        iteration %in% iterations,
-                       select=c("configuration"), drop=TRUE))
-  
-  selection <- iraceResults$allConfigurations[,".ID."] %in% ids
-  
-  if (length(selection) < 1) stop("No configuration found with id", ids,".")
-  
-  configurations <- iraceResults$allConfigurations[selection, , drop=FALSE]
+                       select="configuration", drop=TRUE))
+  get_configuration_by_id_helper (iraceResults$allConfigurations, ids, drop.metadata = drop.metadata)
+}
+
+
+get_configuration_by_id_helper <- function(allConfigurations, ids, drop.metadata)
+{  
+  configurations <- allConfigurations[allConfigurations[[".ID."]] %in% ids, , drop=FALSE]
+  if (nrow(configurations) == 0L)
+    stop("No configuration found with ID:", ids, ".")
   
   if (drop.metadata)
     configurations <- removeConfigurationsMetaData(configurations)
-  return(configurations)
+  configurations
 }
 
---
+++ man-roxygen/arg_drop_metadata.R
@@ -0,0 +1,3 @@
+#' @param drop.metadata (`logical(1)`)\cr Remove metadata, such as the
+#'   configuration ID and the ID of the parent, from the returned
+#'   configurations.  See [removeConfigurationsMetaData()].
