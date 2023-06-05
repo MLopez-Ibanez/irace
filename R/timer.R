b35e97094be731d8f3883d99b84aeed9d5b97bbd
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-06-05 09:49:21 +0100

    * R/timer.R: simplify.

--- R/timer.R
+++ R/timer.R
@@ -5,13 +5,11 @@ Timer <- R6::R6Class("Timer", lock_class = TRUE, list(
   },
   elapsed = function() {
     x <- proc.time() - self$start
-    if (!is.na(x[4L])) 
-        x[1L] <- x[1L] + x[4L]
-    if (!is.na(x[5L])) 
-        x[2L] <- x[2L] + x[5L]
-    x <- x[1L:3L]
-    names(x) <- c("user", "system", "wallclock")
-    x
+    if (!is.na(x[[4L]])) 
+        x[[1L]] <- x[[1L]] + x[[4L]]
+    if (!is.na(x[[5L]])) 
+        x[[2L]] <- x[[2L]] + x[[5L]]
+    c(user=x[[1L]], system=x[[2L]], wallclock=x[[3L]])
   })
 )
 
