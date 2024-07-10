1edd252001b06e1bff60af3098815592fa83fe43
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2024-07-10 14:08:59 +0100

    * R/irace_summarise.R: increase compatibility with older versions.

--- R/irace_summarise.R
+++ R/irace_summarise.R
@@ -17,28 +17,45 @@ irace_summarise <- function(iraceResults)
   if (missing(iraceResults)) stop("argument 'iraceResults' is missing")
   iraceResults <- read_logfile(iraceResults)
 
-  niterations <- length(iraceResults$allElites)
-
-  if (!is.null(iraceResults$state$elapsed)) {
+  if (is.null(iraceResults$state$elapsed)) {
+    time_cpu_user <- time_cpu_sys <- time_cpu_total <- time_wallclock <- NA
+  } else {
     time_cpu_user <- iraceResults$state$elapsed[["user"]]
     time_cpu_sys <- iraceResults$state$elapsed[["system"]]
     time_cpu_total <- time_cpu_user + time_cpu_sys
     time_wallclock <- iraceResults$state$elapsed[["wallclock"]]
-  } else {
-    time_cpu_user <- time_cpu_sys <- time_cpu_total <- time_wallclock <- NA
   }
+
+  n_iterations <- length(iraceResults$allElites)
+  experiment_log <- iraceResults$state$experiment_log
+  if (is.null(experiment_log)) {
+    experiment_log <- iraceResults$experimentLog
+  } else if (is.null(experiment_log))
+    stop("Experiment log is NULL")
+  
+  version <- iraceResults$irace_version
+  if (is.null(version))
+    version <- iraceResults$irace.version
+
+  time_targetrunner <- iraceResults$state$recovery_info$timeUsed
+  if (is.null(time_targetrunner))
+    time_targetrunner <- iraceResults$state$timeUsed
+
+  rejected_ids <- iraceResults$state$rejected_ids
+  if (is.null(rejected_ids))
+    rejected_ids <- iraceResults$state$rejectedIDs
   
   list(
-    version = iraceResults$irace_version,
-    n_iterations = niterations,
+    version = version,
+    n_iterations = n_iterations,
     n_configurations = nrow(iraceResults$allConfigurations),
     n_initial_configurations = if (is.null(iraceResults$scenario$initConfigurations)) 0L else nrow(iraceResults$scenario$initConfigurations),
     n_instances = nrow(iraceResults$experiments),
-    n_experiments = nrow(iraceResults$state$experiment_log),
-    n_elites = length(iraceResults$allElites[[niterations]]),
+    n_experiments = nrow(experiment_log),
+    n_elites = length(iraceResults$allElites[[n_iterations]]),
     n_soft_restarts = sum(iraceResults$softRestart),
-    n_rejected = length(iraceResults$state$rejected_ids),
-    time_targetrunner = iraceResults$state$timeUsed,
+    n_rejected = length(rejected_ids),
+    time_targetrunner = time_targetrunner,
     time_cpu_user = time_cpu_user,
     time_cpu_sys = time_cpu_sys,
     time_cpu_total = time_cpu_total,
