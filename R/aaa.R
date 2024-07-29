1c006bcae4681734c247e6f08ecb8a69157547e4
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2024-07-29 20:06:53 +0100

    * R/aaa.R(update_package_version): Return earlier.

--- R/aaa.R
+++ R/aaa.R
@@ -1,23 +1,21 @@
 update_package_version <- function()
 {
-  dcf <- read.dcf(file = "DESCRIPTION")
-  if (NROW(dcf) < 1L) return(NULL)
+  dcf <- read.dcf(file = "DESCRIPTION", fields=c("Version", "RemoteSha"))
+  if (NROW(dcf) < 1L)
+    return(invisible())
   dcf <- as.list(dcf[1L, ])
-  packageversion <- dcf$Version
   git_rev <- dcf$RemoteSha
-  if (is.null(git_rev))
+  if (is.na(git_rev))
     git_rev <- "unknown"
   git <- Sys.which("git")
-  if (fs::file_exists(".git") && length(git) > 0L
-      && grepl("[0-9a-z]+$", system2(git, "describe --first-parent --always", stdout = TRUE), perl=TRUE)) {
+  if (nchar(git) > 0L && fs::file_exists(".git")
+    && grepl("[0-9a-z]+$", system2(git, "describe --first-parent --always", stdout = TRUE), perl=TRUE)) {
     git_rev <- system2(git, "describe --dirty --first-parent --always --exclude '*'", stdout = TRUE)
   }
   if (git_rev != "unknown") {
-    realversion <- paste0(packageversion, ".", git_rev)
+    realversion <- paste0(dcf$Version, ".", git_rev)
     cat(file='./R/version.R', sep='',
-"#' A character string containing the version of `irace` including git SHA.
-#' @export
-irace_version <- '", realversion, "'\n")
+"#' A character string containing the version of `irace` including git SHA.\n#' @export\nirace_version <- '", realversion, "'\n")
   }
   invisible()
 }
