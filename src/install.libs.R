d006649a3736b04c70dd12d13886c0ebde72722e
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2024-08-06 12:47:42 +0200

    src/install.libs.R: Move executables instead of copying.

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,11 +1,16 @@
-execs <- c(file.path("iracebin", c("irace","ablation")),
-           file.path("dummy", "target-runner-dummy"))
-if (WINDOWS) execs <- paste0(execs, ".exe")
-if (any(file.exists(execs))) {
+files <- c(file.path("iracebin", c("irace","ablation")),
+  file.path("dummy", "target-runner-dummy"))
+
+file_move <- function(from, to) {
+  file.copy(from = from, to = to, overwrite = TRUE)
+  file.remove(from)
+}
+
+if (WINDOWS) files <- paste0(files, ".exe")
+if (any(file.exists(files))) {
   dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
-  #print(dest)
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-  file.copy(execs, dest, overwrite = TRUE)
+  file_move(files, dest)
 } else {
-  stop(execs, " not found!")
+  stop(files, " not found!")
 }
