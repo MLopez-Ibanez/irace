e236d5c7aa75a1f85d219d7f986b666460513042
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2024-12-25 21:22:04 +0100

    * inst/examples/slurm/: Refresh example.

--- inst/examples/slurm/job.sh
+++ inst/examples/slurm/job.sh
@@ -28,5 +28,16 @@
 # To load some software (you can show the list with 'module avail'):
 module load r
 # You may need to load more modules depending on your application.
+
+## Option 1. Call the irace executable:
+~/bin/irace --exec-dir=~/local/irace-execdir/ --parallel $((SLURM_NTASKS_PER_NODE))
+# I usually add a symbolic link from the location where the irace executable is installed to ~/bin/irace. Otherwise, replace ~/bin/irace with the path to the irace executable. It will be something like: ~/local/R_libs/4.1/irace/bin/irace. How to find this path is explained in the user guide and in the quick-install intro: https://mlopez-ibanez.github.io/irace/#installing-the-irace-package
+# You can add additional irace options to the line above (see the user-guide), for example --scenario "tuning-scenario.txt". I added --exec-dir above but you can define the execDir in the scenario file instead. It is important that it is a location that is available for writing from the computing nodes. You may also want to redirect the output of job.sh to some file you can read later. How to do that should be explained in the documentation of your cluster. Also, make sure that you are using the same R version when installing irace than the version that you are loading within job.sh. Otherwise, you may get strange errors.
+
+# MPI: If you want to use mpi, you may need to add something like:
+mpirun -np 1 ~/bin/irace --exec-dir=~/local/irace-execdir/ --parallel $((SLURM_NTASKS-1)) --mpi 1
+# $SLURM_NTASKS is expanded automatically to the value you give to --ntasks=. We subtract one from that value because irace itself consumes one task.
+
+## Option 2: Using an R script
 cd /path_to_irace_R_script
 Rscript ./run_irace.R
--- inst/examples/slurm/run_irace.R
+++ inst/examples/slurm/run_irace.R
@@ -1,25 +1,42 @@
+# This is an example of launching irace from an R script.
 library(irace)
-# initial set up
+# Initial set up. Please check in the documentation of your cluster whether
+# $SLURM_NTASKS_PER_NODE is the correct environment variable to get the number
+# of tasks that can be run in parallel.
 ncores <- as.numeric(Sys.getenv("SLURM_NTASKS_PER_NODE", parallel::detectCores()))
 
-parameter_table <- '...'
+# This needs to be customized by the user.  Instead of defining
+# 'parameter_table' you could use a file 'parameters.txt'.
+parameter_table <- '
+param1 "--param1 " i (1,100)
+param2 "--param2=" c (0,1)
+'
 parameters <- readParameters(text = parameter_table)
 
+# This needs to be customized by the user.  Instead of defining targetRunner as
+# an R function 'my_target_runner' you could use an executable file
+# 'my_target_runner'.
 my_target_runner <- function(experiment, scenario)
 {
   instance <- experiment$instance
   configuration <- experiment$configuration
   seed <- experiment$seed
-  return(list(cost = my_algorithm(instance, configuration, seed)))
+  cost <- my_algorithm(instance, configuration, seed)
+  # You could also return 'time' if you are using 'maxTime' in the scenario.
+  return(list(cost = cost))
 }
-instances <- load_instances() # Some function that loads or defines the instances. You could also use trainInstancesFile or trainInstancesDir in the scenario. 
+
+
+# Some function that loads or defines the instances. You could also use
+# trainInstancesFile or trainInstancesDir in the scenario.
+instances <- load_instances() 
 scenario <- list(targetRunner = my_target_runner,
                  instances = instances,
                  maxExperiments = 200,
-                 parallel = ncores)
+                 parallel = ncores, parameters = parameters)
 
 # check if the scenario is valid (comment out if taking too much time)
-checkIraceScenario(scenario, parameters = parameters)
+checkIraceScenario(scenario)
 
 # run irace
-irace.main(scenario = scenario, parameters = parameters)
+irace_main(scenario = scenario)
