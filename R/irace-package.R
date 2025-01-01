485a8928adb2d0ca4fb21d4225769638d4976d9c
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-01-01 22:03:45 +0000

    * .github/workflows/R-CMD-check.yaml: Update coverage job.

--- R/irace-package.R
+++ R/irace-package.R
@@ -8,9 +8,9 @@
 #' @importFrom R6 R6Class
 #' @importFrom graphics abline axis boxplot par plot points strwidth bxp grid
 #' @importFrom spacefillr generate_sobol_set
-#' 
+#'
 #' @details  License: GPL (>= 2)
-#' 
+#'
 #' @author Maintainers: Manuel López-Ibáñez and Leslie Pérez Cáceres
 #'         \email{irace-package@googlegroups.com}
 #'
@@ -21,12 +21,12 @@
 #'   Thomas Stützle, and Mauro Birattari. The irace package: Iterated
 #'   Racing for Automatic Algorithm Configuration. \emph{Operations Research
 #'   Perspectives}, 2016. \doi{10.1016/j.orp.2016.09.002}
-#'    
+#'
 #'   Manuel López-Ibáñez, Jérémie Dubois-Lacoste, Thomas Stützle, and Mauro
 #'   Birattari. \emph{The irace package, Iterated Race for Automatic
 #'   Algorithm Configuration}. Technical Report TR/IRIDIA/2011-004, IRIDIA,
 #'   Université Libre de Bruxelles, Belgium, 2011.
-#'   
+#'
 #'   Manuel López-Ibáñez and Thomas Stützle. The Automatic Design of
 #'   Multi-Objective Ant Colony Optimization Algorithms. \emph{IEEE Transactions
 #'   on Evolutionary Computation}, 2012.
@@ -38,6 +38,6 @@
 # This silences CRAN NOTE:
 #   Namespace in Imports field not imported from: 'codetools'
 #     All declared Imports should be used.
-.ignore_unused_imports <- function() {
+.ignore_unused_imports <- function() { # nocov start
   codetools::findGlobals
-}
+} # nocov end
--- tests/testthat/helper-common.R
+++ tests/testthat/helper-common.R
@@ -9,7 +9,7 @@ generate_set_seed <- function()
 test_irace_detectCores <- function()
 {
   if (!identical(Sys.getenv("NOT_CRAN"), "true")) return(1L)
-  # FIXME: covr fails in github actions otherwise.
+  # FIXME: covr takes a very long time on github actions otherwise.
   if (identical(Sys.getenv("COVR_COVERAGE"), "true")) return(1L)
   x <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
   if (nzchar(x) && x == "TRUE") return(2L)
@@ -27,20 +27,20 @@ system_os_is_windows <- function() .Platform$OS.type == "windows"
 
 ## Functions ##########################################################
 f_ackley <- function (x,y, nsize = 0.01) {
-  # Transformation of parameter values 
+  # Transformation of parameter values
   # from [0,1] to [vmin,vmax]
   vmin <- -5
   vmax <- 5
   x <- (x*(vmax-vmin)) + vmin
   y <- (y*(vmax-vmin)) + vmin
-  
+
   a <- -20 * exp (-0.2 * sqrt(0.5 * (x^2 + y^2)))
-  b <- exp(0.5 * (cos(2*pi*x) + cos(2*pi*y)))  
+  b <- exp(0.5 * (cos(2*pi*x) + cos(2*pi*y)))
   f <- a - b + exp(1) + 20
   # Simulating stochasticity
   noise <- runif(1, min = (1-nsize), max = (1+nsize))
   f <- f*noise
-  
+
   # Transform result from [fmin,fmax]
   # to [0,100]
   fmin <- 0
@@ -49,13 +49,13 @@ f_ackley <- function (x,y, nsize = 0.01) {
 }
 
 f_goldestein_price <- function (x,y, nsize = 0.01) {
-  # Transformation of parameter values 
+  # Transformation of parameter values
   # from [0,1] to [vmin,vmax]
   vmin <- -2
   vmax <- 2
   x <- (x*(vmax-vmin)) + vmin
   y <- (y*(vmax-vmin)) + vmin
-  
+
   a <- 1 + ((x + y + 1)^2) * (19 - 14*x + 3*x^2 - 14*y + 6*x*y + 3*y^2)
   b <- 30 + ((2*x - 3*y)^2) * (18 - 32*x + 12*x^2 + 48*y - 36*x*y + 27*y^2)
   f <- a*b
@@ -63,7 +63,7 @@ f_goldestein_price <- function (x,y, nsize = 0.01) {
   noise <- runif(1, min = (1-nsize), max = (1+nsize))
   f <- f*noise
 
-  # Transform result from [fmin,fmax] 
+  # Transform result from [fmin,fmax]
   # to [0,100]
   fmin <- 0
   fmax <- 1000000*(1+nsize)
@@ -71,39 +71,39 @@ f_goldestein_price <- function (x,y, nsize = 0.01) {
 }
 
 f_matyas <- function (x,y, nsize = 0.01) {
-  # Trasfomation of parameter values 
+  # Trasfomation of parameter values
   # from [0,1] to [vmin,vmax]
   vmin <- -10
   vmax <- 10
   x <- (x*(vmax-vmin)) + vmin
   y <- (y*(vmax-vmin)) + vmin
-  
+
   f <- 0.26 * (x^2 + y^2) - (0.48*x*y)
   # Simulating stochasticity
   noise <- runif(1, min = (1-nsize), max = (1+nsize))
   f <- f*noise
 
-  # Transform result from [fmin,fmax] 
+  # Transform result from [fmin,fmax]
   # to [0,100]
   fmin <- 0
   fmax <- 100*(1+nsize)
   ((f - fmin) / (fmax-fmin)) * (100-0) + 0
 }
 
-f_himmelblau <- function (x,y, nsize = 0.01) {  
-  # Trasfomation of parameter values 
+f_himmelblau <- function (x,y, nsize = 0.01) {
+  # Trasfomation of parameter values
   # from [0,1] to [vmin,vmax]
   vmin <- -5
   vmax <- 5
   x <- (x*(vmax-vmin)) + vmin
   y <- (y*(vmax-vmin)) + vmin
-  
+
   f <- (x^2 + y - 11)^2 + (x + y^2 - 7)^2
   # Simulating stochasticity
   noise <- runif(1, min = (1-nsize), max = (1+nsize))
   f <- f*noise
-  
-  # Transform result from [fmin,fmax] 
+
+  # Transform result from [fmin,fmax]
   # to [0,100]
   fmin <- 0
   fmax <- 2000*(1+nsize)
@@ -118,13 +118,13 @@ target_runner_capping_xy <- function(experiment, scenario)
 
   x <- configuration[["x"]]
   y <- configuration[["y"]]
-  
+
   value <- switch(instance,
                   ackley     = f_ackley(x, y),
                   goldestein = f_goldestein_price(x, y),
                   matyas     = f_matyas(x, y),
                   himmelblau  = f_himmelblau(x, y))
-  
+
   # Simulate execution bound
   list(cost = value, time=min(value + 0.1, bound))
 }
@@ -143,7 +143,7 @@ irace_capping_xy <- function(..., targetRunner = force(target_runner_capping_xy)
    x "" r (0, 1.00)
    y "" r (0, 1.00)
    reject "" c (0,1)'
-  
+
   parameters <- readParameters(text = parameters_table)
   logFile <- withr::local_tempfile(fileext=".Rdata")
   scenario <- list(instances = c("ackley", "goldestein", "matyas", "himmelblau"),
@@ -158,7 +158,7 @@ irace_capping_xy <- function(..., targetRunner = force(target_runner_capping_xy)
   scenario <- checkScenario (scenario)
 
   expect_true(irace:::checkTargetFiles(scenario = scenario))
-  
+
   confs <- irace(scenario = scenario)
   best_conf <- getFinalElites(scenario$logFile, n = 1L, drop.metadata = TRUE)
   expect_identical(removeConfigurationsMetaData(confs[1L, , drop = FALSE]),
@@ -170,7 +170,7 @@ wrap_target_runner_error <- function(target_runner, limit)
 {
   counter <- force(limit)
   target_runner <- force(target_runner)
-  
+
   fun <- function(experiment, scenario) {
     counter <<- counter - 1L
     if (counter <= 0L)
@@ -185,7 +185,7 @@ wrap_target_runner_counter <- function(target_runner)
 {
   counter <- 0L
   target_runner <- force(target_runner)
-  
+
   fun <- function(experiment, scenario) {
     counter <<- counter + 1L
     target_runner(experiment, scenario)
@@ -193,4 +193,3 @@ wrap_target_runner_counter <- function(target_runner)
   parent.env(environment(fun)) <- globalenv()
   fun
 }
-
